3d49efe84903f6cf89582716f263aef681b487ed
danielkirwa <kirwadan03@gmail.com>
2021-02-10 00:49:02 +0300

    modified file

--- scripts/account.js
+++ scripts/account.js
@@ -1,7 +1,22 @@
 
 // global variable 
 
-let callpopupmenu = document.querySelector('.weeked-discription');
+let callpopupmenu = document.querySelector('.profileicon');
 callpopupmenu.addEventListener('click' , () =>{
-	alert('working');
+	let menu = document.querySelector('.popupmenu');
+	
+	let popupmenucode = localStorage.getItem('popupmenu');
+	if (popupmenucode == undefined) {
+		localStorage.setItem('popupmenu', 1);
+		menu.style.display = "block";
+	}else{
+	if (popupmenucode == 1) {
+		menu.style.display = "none";
+		localStorage.setItem('popupmenu', 0);
+	}else{
+		localStorage.setItem('popupmenu', 1);
+		menu.style.display = "block";
+	}
+	}
+
 })
