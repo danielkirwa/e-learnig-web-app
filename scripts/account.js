29ce5225f79feefd8a1e2b28ceba6c87241cb578
danielkirwa <kirwadan03@gmail.com>
2021-03-17 16:20:33 +0300

    fetching quiz complete and grading complete yet to fix quiz selector

--- scripts/account.js
+++ scripts/account.js
@@ -13,7 +13,10 @@ let btnlogout = document.getElementById('btnlogout');
 let quizcode ;
 let indexedEmail;
 let fetchedphone,fetchedfname,fetchedmname,fetchedlname,fetchedintrest;
-
+let displayweb = document.querySelector('.div100');
+let displayjava = document.querySelector('.div200');
+let displaypython = document.querySelector('.div300');
+let displayandroid = document.querySelector('.div400');
 
 btncloseenrollpopup.addEventListener('click', () => {
 	
@@ -62,37 +65,70 @@ btnsubmitenroll.addEventListener('click' , ()  => {
 	let skilllevel = document.getElementById('skilllevel');
 	var selectedlevel = skilllevel.options[skilllevel.selectedIndex].text;
 
-	 console.log(selectedcourse + selectedlevel);
-	let myctivityframe = document.querySelector('.enrolled');
-	  let newactivityframe = `
-	  		<div class="enrolled-activity">
-		 	<div class="activitydisplay">
-				<center>
-				<span id="coursecode">${selectedcoursecode}</span>	${selectedcourse}
-				</center>
-			</div><br>
-			 <label>Score : 0.0</label>
-			<p>
-				Just started <br>
-				<span>Level :</span>
-			<span> ${selectedlevel}</span> <br><br>
-			<button id="openquiz" onclick="openquizpage()">Open Quiz </button>
-			</p>
-		 </div><br>
-	  `
-	 myctivityframe.innerHTML += newactivityframe;
-	 	 alert("added");
+	console.log(selectedcourse);
+	console.log(selectedcoursecode);
+	console.log(selectedlevel);
+
+	if (selectedcoursecode == 100) {
+		displayweb.style.display = "block";
+		localStorage.setItem('webcode', 100);
+	}else if (selectedcoursecode == 200) {
+		displayjava.style.display = "block";
+		localStorage.setItem('javacode', 200);
+	}else if (selectedcoursecode == 300) {
+		displaypython.style.display = "block";
+		localStorage.setItem('pythoncode', 300);
+	}else if (selectedcoursecode == 400) {
+		displayandroid.style.display = "block";
+		localStorage.setItem('androidcode', 400);
+	}else  {
+		alert('course not available');
+	}
+
+
 	 	 blurbody.style.opacity = "1";
 	 	 enrollform.style.display ="none";
 
 	 	
 })
 
-  function openquizpage() {
+// retain course display 
+
+function retaincoursedisplay() {
+	// body...
+	let webcode =  localStorage.getItem('webcode');
+	let javacode =  localStorage.getItem('javacode');
+	let pythoncode =  localStorage.getItem('pythoncode');
+	let androidcode =  localStorage.getItem('androidcode');
+	 console.log(webcode  + javacode + pythoncode + androidcode);
+// first check
+		if (webcode == undefined) {
+	}else{
+		displayweb.style.display = "block";		
+	}
+// second check
+		if (javacode == undefined) {
+	}else{
+		displayjava.style.display = "block";		
+	}
+// third check
+	if (pythoncode == undefined) {
+	}else{
+		displaypython.style.display = "block";		
+	}
+// forth check 
+	if (androidcode == undefined) {
+	}else{
+		displayandroid.style.display = "block";		
+	}
+}
+retaincoursedisplay() ;
+
+  function openquizpage1() {
   	// body...
 
-  		let courseid = document.getElementById('courseid').innerHTML;
-  		let quizno = document.getElementById('quizno').innerHTML;
+  		let courseid = document.getElementById('courseid1').innerHTML;
+  		let quizno = document.getElementById('quizno1').innerHTML;
  
   		 quizcode = courseid + quizno;
   		  console.log(quizcode);
@@ -104,7 +140,31 @@ btnsubmitenroll.addEventListener('click' , ()  => {
   	// body...
 
   		let courseid = document.getElementById('courseid2').innerHTML;
-  		let quizno = document.getElementById('quizno').innerHTML;
+  		let quizno = document.getElementById('quizno2').innerHTML;
+ 
+  		 quizcode = courseid + quizno;
+  		  console.log(quizcode);
+  		   localStorage.setItem('quizcode' , quizcode);
+  	 	alert('Open Quiz ' + quizcode);
+  	 	window.location.href= 'quizapp.html';
+  }
+  function openquizpage3() {
+  	// body...
+
+  		let courseid = document.getElementById('courseid3').innerHTML;
+  		let quizno = document.getElementById('quizno3').innerHTML;
+ 
+  		 quizcode = courseid + quizno;
+  		  console.log(quizcode);
+  		   localStorage.setItem('quizcode' , quizcode);
+  	 	alert('Open Quiz ' + quizcode);
+  	 	window.location.href= 'quizapp.html';
+  }
+  function openquizpage4() {
+  	// body...
+
+  		let courseid = document.getElementById('courseid4').innerHTML;
+  		let quizno = document.getElementById('quizno4').innerHTML;
  
   		 quizcode = courseid + quizno;
   		  console.log(quizcode);
