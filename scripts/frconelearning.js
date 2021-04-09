72196ca29a38d3fb33c40e8db937b0a44815ccfb
danielkirwa <kirwadan03@gmail.com>
2021-04-09 18:43:44 +0300

    Enrolled courses selected from database  fixed completely

--- scripts/frconelearning.js
+++ scripts/frconelearning.js
@@ -1,20 +1,2 @@
-  // Your web app's Firebase configuration
-
-      var firebaseConfig = {
-    apiKey: "AIzaSyArL98ueDDOZhgnS0Z9PAOfKgP8MUcrho0",
-    authDomain: "e-learning-app-d98c7.firebaseapp.com",
-    databaseURL: "https://e-learning-app-d98c7-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "e-learning-app-d98c7",
-    storageBucket: "e-learning-app-d98c7.appspot.com",
-    messagingSenderId: "17597297186",
-    appId: "1:17597297186:web:e724ebb9903c428f96907b"
-  };
-
-
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-   const auth = firebase.auth();
-
 
 
